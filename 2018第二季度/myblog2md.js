6f0491c8415f72cda5e4d4e716887c37dfb7d582
JinHengyu <2195868682@qq.com>
2018-07-01 10:52:47 +0800

    oh,thars better

--- 2018第二季度/myblog2md.js
+++ 2018第二季度/myblog2md.js
@@ -23,3 +23,7 @@ connection.query('SELECT * from t_article', function (error, results, fields) {
 // 引用是保存在栈上，对象是保存在堆中.
 // 基础数据类型和引用变量都是值传递(完全拷贝)
 // 函数参数无论如何都是在栈中创建一个新的变量
+
+
+
+// alert方法是浏览器级别的, 和浏览器通知一样, 不是网页级别的, 所以无法js控制alert窗口...
--- 2018第二季度/文件名包含标点符号.js
+++ 2018第二季度/文件名包含标点符号.js
@@ -1,2 +1,5 @@
 const fs = require('fs')
 fs.writeFileSync('./test:haha.txt', 'hello node')
+
+
+// 编译时发生的异常通常都是在语义分析阶段...
